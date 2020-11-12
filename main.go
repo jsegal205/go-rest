6a54c980ad7966fc98fb35ec1201029d2851adfc
Jim Segal <hello@jimsegal.com>
2020-11-11 20:51:08 -0600

    format with vscode go extension

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"encoding/json"
 )
 
 type Recipe struct {
-	Title string `json:"Title"`
+	Title       string `json:"Title"`
 	Ingredients string `json:"Ingredients"`
-	Directions string `json:"Directions"`
+	Directions  string `json:"Directions"`
 }
 
 type Recipes []Recipe
 
 func allRecipes(w http.ResponseWriter, r *http.Request) {
 	recipes := Recipes{
-		Recipe{Title:"really bad cookies", Ingredients: "flour, eggs, milk, sugar", Directions: "combine all the things and bake"},
-		Recipe{Title:"ham", Ingredients: "ham", Directions: "just cut a slice and eat it"},
+		Recipe{Title: "really bad cookies", Ingredients: "flour, eggs, milk, sugar", Directions: "combine all the things and bake"},
+		Recipe{Title: "ham", Ingredients: "ham", Directions: "just cut a slice and eat it"},
 	}
 
 	fmt.Println("endpoint hit:: all Recipes")
@@ -26,15 +26,15 @@ func allRecipes(w http.ResponseWriter, r *http.Request) {
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	  fmt.Fprintf(w, "Hello jimbo jellybeans")
+	fmt.Fprintf(w, "Hello jimbo jellybeans")
 }
 
 func handleRequests() {
-		http.HandleFunc("/", homePage)
-		http.HandleFunc("/recipes", allRecipes)
-		log.Fatal(http.ListenAndServe(":8081", nil))
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/recipes", allRecipes)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func main() {
-	  handleRequests()
+	handleRequests()
 }
