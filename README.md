1ce358670661c2c57718604728ac96c7a48c0655
Jim Segal <hello@jimsegal.com>
2020-11-11 20:45:36 -0600

    init commit with basic GET route

---
+++ README.md
@@ -0,0 +1,11 @@
+# GO-REST
+
+A small little webserver written in Go to learn about Go.
+
+## Run
+
+```go
+go run main.go
+```
+
+Then navigate to [http://localhost:8081](http://localhost:8081)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+)
+
+type Recipe struct {
+	Title string `json:"Title"`
+	Ingredients string `json:"Ingredients"`
+	Directions string `json:"Directions"`
+}
+
+type Recipes []Recipe
+
+func allRecipes(w http.ResponseWriter, r *http.Request) {
+	recipes := Recipes{
+		Recipe{Title:"really bad cookies", Ingredients: "flour, eggs, milk, sugar", Directions: "combine all the things and bake"},
+		Recipe{Title:"ham", Ingredients: "ham", Directions: "just cut a slice and eat it"},
+	}
+
+	fmt.Println("endpoint hit:: all Recipes")
+	json.NewEncoder(w).Encode(recipes)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	  fmt.Fprintf(w, "Hello jimbo jellybeans")
+}
+
+func handleRequests() {
+		http.HandleFunc("/", homePage)
+		http.HandleFunc("/recipes", allRecipes)
+		log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	  handleRequests()
+}
